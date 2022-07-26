8ebb906166abe620a8706f2b7f0d527722282a26
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-26 15:32:45 +0100

    Add files via upload

---
+++ host.h
@@ -0,0 +1,21 @@
+#ifndef HOST_H_
+#define HOST_H_
+
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include <fstream>
+#include "ap_int.h"
+
+#define DTYPE_LENGTH 32
+typedef ap_int<DTYPE_LENGTH> DTYPE;
+typedef ap_int<32> DTYPE_OUT;
+
+typedef unsigned int u32;
+
+
+
+
+#endif
